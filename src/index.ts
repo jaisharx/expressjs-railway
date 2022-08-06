d593b1cd8cca3452e33be60b9e74447eaf52801b
Jai Sharma <sendtojsharma@gmail.com>
2022-08-06 21:16:15 +0530

    Adding routes

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-import bodyParser from "body-parser";
-import express from "express";
+import bodyParser from 'body-parser'
+import express from 'express'
 
-const app = express();
-const port = process.env.PORT || 3333;
+const app = express()
+const port = process.env.PORT || 3333
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(bodyParser.json())
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.text({ type: 'text/html' }))
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
+  res.json({
+    status: 'Ok',
+  })
+})
+
+app.get('/about', async (req, res) => {
   res.json({
     name: 'Jai Sharma',
     age: 21,
-    country: 'India'
-  });
-});
+    country: 'India',
+  })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
